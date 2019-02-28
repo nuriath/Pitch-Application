788dfeed067bb79c4b24ff15414a46e508064ce4
nuriath <mnuriath28@gmail.com>
2019-02-28 09:49:43 +0200

    edit a pitch

--- app/main/views.py
+++ app/main/views.py
@@ -56,8 +56,8 @@ def update_pic(uname):
        db.session.commit()
    return redirect(url_for('main.profile',uname=uname))
 
-# @main.route('/music/')
-# def music():
+# @main.route('/music/pitch')
+# def music(pitch):
     
 #     return render_template('index.html')
 
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export MAIL_USERNAME = mnuriath28
-export MAIL_PASSWORD = J0786154788
+export MAIL_USERNAME = 'mnuriath28'
+export MAIL_PASSWORD = 'J0786154788'
 export SECRET_KEY='J2307'
 
 python3.6 manage.py server
